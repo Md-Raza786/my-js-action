10f8a9dc27656f2f1b67eb82f713711893713139
Md-Raza786 <89716670+Md-Raza786@users.noreply.github.com>
2025-12-18 12:09:42 +0530

    Writing content to index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const core = require('@actions/core');
+
+try {
+  // Read input
+  const name = core.getInput('name');
+
+  // Print message
+  console.log(`Hello, ${name}!`);
+} catch (error) {
+  core.setFailed(error.message);
+}
 
